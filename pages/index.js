fa63fe3c4c514dcff9d5fa626e2dddcb47cf8003
Karanja Mutahi <mutahi.henry@gmail.com>
2020-05-10 23:05:31 +0300

    Deployment Changes
    Testing Heroku deployment

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Items from '../components/Items';
-import React from 'react';
 
-const version = React.version;
+//const version = React.version;
 
 const Home = props => {
-    console.log(`React version = ${version}`);
+    //console.log(`React version = ${version}`);
     return (
     <div>
         <Items page={parseInt(props.query.page) || 1}/>
