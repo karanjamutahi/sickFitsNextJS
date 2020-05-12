78b87083dd32b41f30b1ac743f41e34f1fb6501a
Karanja Mutahi <mutahi.henry@gmail.com>
2020-05-12 22:16:19 +0300

    Changed Endpoints

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const devEndpoint = `http://localhost:4444`;
 export const perPage = 4;
-export const prodEndpoint = `https://karanja-sickfits-backend.herokuapp.com/`;
+export const prodEndpoint = `http://3ca3e313.ngrok.io/`;
