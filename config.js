baab141398a190c0e2cb8972acdbbf79f0debc3a
Karanja Mutahi <mutahi.henry@gmail.com>
2020-05-12 22:37:56 +0300

    Changed test endpoint to https

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const devEndpoint = `http://localhost:4444`;
 export const perPage = 4;
-export const prodEndpoint = `http://3ca3e313.ngrok.io/`;
+export const prodEndpoint = `https://3ca3e313.ngrok.io/`;
